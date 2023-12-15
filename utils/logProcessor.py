95c953f2f3f868addd8dc8cf3228d118bef50bf3
Matthew Ramanah <matthew.ramanah@epochcapital.com.au>
2023-12-15 12:24:48 +0800

    comitting modelState.json

--- utils/logProcessor.py
+++ utils/logProcessor.py
@@ -7,6 +7,6 @@ with open(cfg_file, 'r') as f:
 logs = pta.loadLogs(cfg)
 logs = pta.formatLogs(logs)
 
-symsToPlot = ['AP0']#, "RS0", "ALSI0", "FTI0", "ZW0", "KE0"]
+symsToPlot = ['ZF0']  # , "RS0", "ALSI0", "FTI0", "ZW0", "KE0"]
 pta.plotLogs(logs, symsToPlot)
 lg.info("Completed.")
