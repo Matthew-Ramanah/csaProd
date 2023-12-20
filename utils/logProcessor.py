daf57ca6d8c48fb32ec17b1eef7a41dc02abc040
Matthew Ramanah <matthew.ramanah@epochcapital.com.au>
2023-12-20 08:18:21 +0800

    modelState.json

--- utils/logProcessor.py
+++ utils/logProcessor.py
@@ -7,6 +7,6 @@ with open(cfg_file, 'r') as f:
 logs = pta.loadLogs(cfg)
 alphasLogs = pta.loadAlphasLogs(cfg)
 
-symsToPlot = ["ICE-US_KC0", "RS0", "AP0", "ALSI0", "ICE-US_CT0", "KE0"]
+symsToPlot = ["ICE-US_KC0"]
 pta.plotLogs(logs, alphasLogs, symsToPlot)
 lg.info("Completed.")
