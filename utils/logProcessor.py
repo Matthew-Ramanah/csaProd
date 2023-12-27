39dcf90e1ea43bdab0aaae1215ef2c3f30d57bc1
Matthew Ramanah <matthew.ramanah@epochcapital.com.au>
2023-12-28 05:37:48 +0800

    modelState.json

--- utils/logProcessor.py
+++ utils/logProcessor.py
@@ -9,6 +9,6 @@ logDir = logRoot  # paperLogRoot  #
 logs = pta.loadLogs(cfg, logDir)
 alphasLogs = pta.loadAlphasLogs(cfg, logDir)
 
-symsToPlot = cfg['targets']
+symsToPlot = ["ALSI0"]  # cfg['targets']
 pta.plotLogs(cfg, logs, alphasLogs, symsToPlot)
 lg.info("Completed.")
