f83d0f3ad5c3366c0aa0d9d2427141ab38dfcd59
Matthew Ramanah <matthew.ramanah@epochcapital.com.au>
2024-01-24 07:16:19 +0800

    fitting assetModel

--- utils/logProcessor.py
+++ utils/logProcessor.py
@@ -10,6 +10,6 @@ logs = pta.loadLogs(cfg, logDir, AFBI.timezone)
 alphasLogs = pta.loadAlphasLogs(cfg, logDir, AFBI.timezone)
 tradeLogs = pta.loadTradeLogs(cfg, timezone=AFBI.timezone)
 
-symsToPlot = cfg['targets']
+symsToPlot = ["ICE-US_CT0", "AP0"]  # cfg['targets']
 pta.plotLogs(cfg, logs, alphasLogs, tradeLogs, symsToPlot)
 lg.info("Completed.")
